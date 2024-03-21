ef0845606c415ca93a552209e86c5f5b775d7c6c
OksankaITshnik <pshibilaoksanka@gmail.com>
2024-03-21 14:03:52 +0100

    add hello ukraine and 4-9

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Ukraine!");
+        for (int i = 4; i<10; i++){
+            System.out.println(i);
+        }
     }
 }
