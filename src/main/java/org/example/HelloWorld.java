06eef1aa51eb33c587e6d493d53f2a25de97993d
OksankaITshnik <pshibilaoksanka@gmail.com>
2024-03-21 15:45:39 +0100

    ya

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Ukraine!");
-        for (int i = 4; i<10; i++){
+        System.out.println("Hello spain");
+        for (int i = 0; i<3; i++){
             System.out.println(i);
         }
     }
